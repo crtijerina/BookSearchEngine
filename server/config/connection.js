ab23cad366201690c16cb9115d8e20eb6f097139
Chris Tijerina <crtijerina1988@gmail.com>
2021-11-18 23:06:58 -0600

    added love

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksearchengine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Chris:12345%21%40%23%24%25cluster0.7brun.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
