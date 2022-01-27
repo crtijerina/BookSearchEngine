5fa2c35815958aaccc27829f0f1bfa9127d79696
Chris Tijerina <crtijerina1988@gmail.com>
2022-01-27 17:27:17 -0600

    made changes to savedbooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import {
-  Jumbotron,
-  Container,
-  CardColumns,
-  Card,
-  Button,
-} from 'react-bootstrap';
-
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_ME } from '../utils/queries';
+import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import { removeBookId } from '../utils/localStorage';
-
 import Auth from '../utils/auth';
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(QUERY_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK)
+  const { loading, error, data } = useQuery(GET_ME);
+  
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
-  const userData = data?.me || {};
+  const userData = data?.me;
 
-  // yes this done.
   const handleDeleteBook = async (bookId) => {
     
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -30,11 +24,11 @@ const SavedBooks = () => {
     }
 
     try {
-      const { data } = await removeBook({
-        variables: { bookId },
+      await removeBook({
+        variables: { bookId: bookId },
       });
 
-      // I need to remeber to remove book's id from local storage, I think  this will be a good point to stop.
+    
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
@@ -42,7 +36,7 @@ const SavedBooks = () => {
   };
 
   if (loading) {
-    return <h2>LOADING BOOK DATA...</h2>;
+    return <h2>Checking BOOK INVENTORY...</h2>;
   }
 
   return (
@@ -54,30 +48,22 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks?.length
+          {userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'book' : 'books'
-              }:`
+                userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks?.map((book) => {
+          {userData.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border="dark">
-                {book.image ? (
-                  <Card.Img
-                    src={book.image}
-                    alt={`The cover for ${book.title}`}
-                    variant="top"
-                  />
-                ) : null}
+                {book.image ? <Card.Img src={book.image}
+                    alt={`${book.title} front cover`} variant="top" /> : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteBook(book.bookId)}
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}
                   >
                     Delete this Book!
                   </Button>
