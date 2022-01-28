d0962e51c52cd751065755e8f220e0b945187e63
Chris Tijerina <crtijerina1988@gmail.com>
2022-01-27 19:35:34 -0600

    adding more to API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,15 @@
 export const getMe = (token) => {
-    headers: return fetch('/api/users/me', {
-       headers: {  'Content-Type'
+    return fetch('/api/users/me', {
+       headers: {
          'Content-Type': 'application/json',
-         aut: `Bearer $[token]$`,
-     });
+         aut: `Bearer ${token}$`,
+       }
+        });
    };
 
-export const createUser =
+export const createUser = (userData) => {
+    return fetch('/api/users', {
+      method: 'POST',
 
 export const loginUser = 
 
